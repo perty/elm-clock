7aeec62e4016e412432cb3d2c47805a4d9768878
Per Lundholm <per@artcomputer.se>
2019-11-17 16:06:33 +0100

    Refactor the central time conversion to one place.

--- src/Main.elm
+++ src/Main.elm
@@ -21,13 +21,20 @@ main =
 -- MODEL
 
 
+type alias Date =
+    { hour : Int
+    , minute : Int
+    , second : Int
+    }
+
+
 type alias Model =
-    Time.Posix
+    Date
 
 
 init : () -> ( Model, Cmd Msg )
 init _ =
-    ( Time.millisToPosix 0, Cmd.none )
+    ( Date 0 0 0, Cmd.none )
 
 
 
@@ -42,7 +49,12 @@ update : Msg -> Model -> ( Model, Cmd Msg )
 update msg _ =
     case msg of
         Tick newTime ->
-            ( newTime, Cmd.none )
+            ( dateFromPosix newTime, Cmd.none )
+
+
+dateFromPosix : Time.Posix -> Date
+dateFromPosix time =
+    Date (Time.toHour Time.utc time) (Time.toMinute Time.utc time) (Time.toSecond Time.utc time)
 
 
 
@@ -70,17 +82,17 @@ view model =
         , style "max-width" "100px"
         ]
         [ svg [ viewBox "0 0 100 100", width "100px" ]
-            (clockFn model Time.utc)
+            (clockFn model)
         ]
 
 
-clockFn : Time.Posix -> Time.Zone -> List (Svg Msg)
-clockFn theDateNow zone =
+clockFn : Date -> List (Svg Msg)
+clockFn theDateNow =
     [ g [ fill "black", stroke "black", transform "translate(50,50)" ]
         (clockFace
-            ++ hourHand theDateNow zone
-            ++ minutesHand theDateNow zone
-            ++ secondsHand theDateNow zone
+            ++ hourHand theDateNow
+            ++ minutesHand theDateNow
+            ++ secondsHand theDateNow
         )
     ]
 
@@ -108,7 +120,7 @@ tick minute =
                 "42"
 
         angle =
-            toString (minuteToAngle minute)
+            String.fromFloat (minuteToAngle minute)
     in
     line [ x1 tickStart, y1 "0", x2 "49", y2 "0", transform ("rotate(" ++ angle ++ ")") ] []
 
@@ -118,53 +130,48 @@ minuteToAngle minute =
     (toFloat minute - 15.0) * 360 / 60.0
 
 
-hourHand : Time.Posix -> Time.Zone -> List (Svg Msg)
-hourHand theDateNow zone =
+hourHand : Date -> List (Svg Msg)
+hourHand theDateNow =
     let
         angle =
-            minuteToAngle (hourMinute theDateNow zone)
+            minuteToAngle (hourMinute theDateNow)
     in
-    [ g [ transform ("rotate(" ++ toString angle ++ ")") ]
+    [ g [ transform ("rotate(" ++ String.fromFloat angle ++ ")") ]
         [ polygon [ points "0,0 4,4 20,0 4, -4", transform "translate(10,0)" ] []
         , xLine "0" "10"
         ]
     ]
 
 
-toString : Float -> String
-toString f =
-    String.fromFloat f
-
-
 
 -- Which minute should the hour hand point to?
 
 
-hourMinute : Time.Posix -> Time.Zone -> Int
-hourMinute theDateNow zone =
-    modBy 12 (Time.toHour zone theDateNow) * 5 + Time.toMinute zone theDateNow // 12
+hourMinute : Date -> Int
+hourMinute theDateNow =
+    modBy 12 theDateNow.hour * 5 + theDateNow.minute // 12
 
 
-minutesHand : Time.Posix -> Time.Zone -> List (Svg Msg)
-minutesHand theDateNow zone =
+minutesHand : Date -> List (Svg Msg)
+minutesHand theDateNow =
     let
         angle =
-            minuteToAngle (Time.toMinute zone theDateNow)
+            minuteToAngle theDateNow.minute
     in
-    [ g [ transform ("rotate(" ++ toString angle ++ ")") ]
+    [ g [ transform ("rotate(" ++ String.fromFloat angle ++ ")") ]
         [ polygon [ points "0,0 2,2 30,0 2, -2", transform "translate(5,0)" ] []
         , xLine "0" "5"
         ]
     ]
 
 
-secondsHand : Time.Posix -> Time.Zone -> List (Svg Msg)
-secondsHand theDateNow zone =
+secondsHand : Date -> List (Svg Msg)
+secondsHand theDateNow =
     let
         angle =
-            minuteToAngle (Time.toSecond zone theDateNow)
+            minuteToAngle theDateNow.second
     in
-    [ g [ transform ("rotate(" ++ toString angle ++ ")") ]
+    [ g [ transform ("rotate(" ++ String.fromFloat angle ++ ")") ]
         [ xLine "-5" "40"
         , circle [ cx "0", cy "0", r "5", fillOpacity "0", transform "translate(30,0)" ] []
         ]
